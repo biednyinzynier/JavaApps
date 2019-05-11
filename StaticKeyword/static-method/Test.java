e54ae3982b21f14531218594510c05bbf27ad724
Burak <46630197+biednyinzynier@users.noreply.github.com>
2019-05-11 16:15:52 +0300

    Update Test.java

--- StaticKeyword/static-method/Test.java
+++ StaticKeyword/static-method/Test.java
@@ -14,6 +14,12 @@ public class Test {
 		Product product3 = new Product(3, "", 1500);
 		manager.add(product3);
 		
+		hello();
 	}
-
+	
+	// If we don't declare as static this method we can't access directly
+	// Because main has already exists. We can't call a non existing methods in an existing method
+	public static void hello() {
+		System.out.println("Hello");
+	}
 }
