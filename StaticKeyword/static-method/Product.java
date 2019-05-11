68c8fd38ad9ca57f23385de6a673b656bdf7d416
Burak <46630197+biednyinzynier@users.noreply.github.com>
2019-05-11 15:52:09 +0300

    Update Product.java

--- StaticKeyword/static-method/Product.java
+++ StaticKeyword/static-method/Product.java
@@ -1,12 +1,12 @@
 package com.burak.statickeyword;
 
 public class Product {
-	int id;
-	String name;
+	static int id;	// if we declare id as static
+	String name; 
 	double price;
 	
 	public Product(int id, String name, double price) {
-		this.id = id;
+		Product.id = id;	// can't use this or super because it doesn't belong object it belongs the class.
 		this.name = name;
 		this.price = price;
 	}
